f6a402eb4128ff6b38868dfb42e7c7a035a7cd19
SMalinge <84065363+SMalinge@users.noreply.github.com>
2021-05-14 13:15:41 +0200

    Add files via upload

---
+++ ProgrammingIsFun.java
@@ -0,0 +1,16 @@
+/**********************************************************************
+* HelloGUI.java
+*RMalinge
+*
+*One of the programs of chapter. Exhibits
+*the use of the JOptionPane GUI
+***********************************************************************/
+
+import javax.swing.JOptionPane;
+public class ProgrammingIsFun
+{
+	public static void main(string[] args)
+	{
+		JOptionPane.showMessageDialog(null, "Programming is Fun!");
+	}
+}
